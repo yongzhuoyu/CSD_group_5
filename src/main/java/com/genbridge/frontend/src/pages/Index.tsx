73474a32b0f67575f68bd08b985ab5c0ba20a741
yujjjia <yujia.ngm.2024@smu.edu.sg>
2026-02-26 00:24:31 +0800

    changed 'Start Learning' button for user who are logged in

--- src/main/java/com/genbridge/frontend/src/pages/Index.tsx
+++ src/main/java/com/genbridge/frontend/src/pages/Index.tsx
@@ -41,7 +41,7 @@ const Index = () => {
       <Navbar />
 
       {/* Hero */}
-      <section className="pt-28 pb-16 px-4">
+      <section className={`pt-28 px-4 ${isLoggedIn ? "pb-8" : "pb-16"}`}>
         <div className="container mx-auto max-w-6xl">
           <div className="max-w-3xl mx-auto text-center">
             <motion.div
@@ -57,9 +57,9 @@ const Index = () => {
                 The self-learning platform that helps you understand the language, memes, and culture defining the next generation.
               </p>
               <div className="flex flex-wrap gap-4 justify-center">
-                <Button size="lg" asChild>
+                <Button size="lg" className={isLoggedIn ? "text-lg px-10 py-6 h-auto" : ""} asChild>
                   <Link to="/register">
-                    Start Learning Free
+                    {isLoggedIn ? "Start Learning" : "Start Learning Free"}
                     <ArrowRight className="w-4 h-4 ml-2" />
                   </Link>
                 </Button>
